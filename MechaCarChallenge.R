000786a8cae9a594ac41f6586ef05add6f3db090
Salih Mohammed <76082772+Donik22@users.noreply.github.com>
2021-07-12 03:27:27 -0700

    Added to R-Script file

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,8 @@
 library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+## Deliverable 1
 
 MechaCar <- read.csv("MechaCar_mpg.csv")
 
@@ -7,3 +11,17 @@ MechaCar
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD , MechaCar)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD , MechaCar))
+
+## Deliverable 2
+
+Suspension_coil <- read.csv("Suspension_Coil.csv")
+
+Suspension_coil
+
+total_summary <- Suspension_coil %>% summarize(Mean = mean(PSI), median(PSI),var(PSI),sd(PSI))
+
+total_summary
+
+lot_summary <- Suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), median(PSI),var(PSI),sd(PSI))
+
+lot_summary
