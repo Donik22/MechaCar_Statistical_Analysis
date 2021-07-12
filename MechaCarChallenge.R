5062e87c8d178afcb817329b50d2d091795197f8
Salih Mohammed <76082772+Donik22@users.noreply.github.com>
2021-07-12 02:44:04 -0700

    adding data and R script

---
+++ MechaCarChallenge.R
@@ -0,0 +1,9 @@
+library(dplyr)
+
+MechaCar <- read.csv("MechaCar_mpg.csv")
+
+MechaCar
+
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD , MechaCar)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD , MechaCar))
